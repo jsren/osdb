f752c761391b6d52272f7c0df8d4dff48ceaba35
James Renwick <jsrenwick3@gmail.com>
2018-05-19 18:06:36 +0100

    Fix b-tree iteration   - range_start/end now in namespace   - Allow reverse iteration   - end() now points beyond final item   - Add more tests   - Fix search_range overloading

--- btree.hpp
+++ btree.hpp
@@ -8,6 +8,9 @@
 
 namespace osdb
 {
+    struct range_start { };
+    struct range_end { };
+
     template<typename T, typename Key, size_t Order, size_t LeafSize>
     class bplus_node;
 
@@ -63,6 +66,7 @@ namespace osdb
     {
         friend bplus_tree<Key, Value, Order, LeafSize>;
         static_assert(Order % 2 == 0, "Order must be a factor of two");
+        static_assert(Order <= std::numeric_limits<size_t>::max() / 2, "");
 
     public:
         using key_type = Key;
@@ -70,23 +74,114 @@ namespace osdb
     private:
         using leaf_type = bplus_leaf<Key, Value, Order, LeafSize>;
 
-        bplus_node* parent{};
-        bool hasLeaves{};
-
-        bplus_node* leftSibling;
-        bplus_node* rightSibling;
-
         union element
         {
             leaf_type* leaf;
             bplus_node* node;
         };
 
+
+        bplus_node* parent{};
+
+        struct {
+            size_t parentIndex : ((sizeof(size_t)*8)-1);
+            bool hasLeaves : 1;
+        } _data{};
+
         std::array<key_type, Order> keys{};
         std::array<element, Order + 1> nodes{};
 
-        bplus_node(bplus_node* parent, bool hasLeaves, bplus_node* left, bplus_node* right)
-            : parent(parent), hasLeaves(hasLeaves), leftSibling(left), rightSibling(right) { }
+        bplus_node(bplus_node* parent, size_t parentIndex, bool hasLeaves)
+            : parent(parent)
+        {
+            _data.hasLeaves = hasLeaves;
+            _data.parentIndex = parentIndex;
+        }
+
+    public:
+        bplus_node(const bplus_node&) = delete;
+        bplus_node(bplus_node&&) = default;
+        bplus_node& operator =(const bplus_node&) = delete;
+        bplus_node& operator =(bplus_node&&) = default;
+
+        ~bplus_node()
+        {
+            if (_data.hasLeaves)
+            {
+                for (auto& elem : nodes) {
+                    if (elem.leaf != nullptr) delete elem.leaf;
+                }
+            }
+            else
+            {
+                for (auto& elem : nodes) {
+                    if (elem.node != nullptr) delete elem.node;
+                }
+            }
+        }
+
+    private:
+        leaf_type* prev_leaf() noexcept
+        {
+            if (parent == nullptr) return nullptr;
+            if (_data.parentIndex == 0) return parent->prev_leaf();
+
+            bplus_node* node = parent->nodes[_data.parentIndex - 1].node;
+            while (true)
+            {
+                if (node->_data.hasLeaves)
+                {
+                    for (size_t i = Order; i != 0; i--)
+                    {
+                        if (node->nodes[i-1].leaf != nullptr) {
+                            return node->nodes[i-1].leaf;
+                        }
+                    }
+                }
+                else
+                {
+                    for (size_t i = Order; i != 0; i--)
+                    {
+                        if (node->nodes[i-1].node != nullptr) {
+                            node = node->nodes[i].node; break;
+                        }
+                    }
+                }
+            }
+            return nullptr;
+        }
+
+        leaf_type& insert_leaf(size_t index, leaf_type*& first, leaf_type*& last)
+        {
+            // Get left and right leaves
+            leaf_type* right;
+            leaf_type* left = index == 0 ? prev_leaf() : nodes[index - 1].leaf;
+
+            if (left != nullptr) {
+                right = left->rightLeaf;
+            }
+            else right = first;
+
+            // Create leaf
+            auto* leaf = new leaf_type(this, left, right);
+
+            // Update right and left in linked list
+            if (left != nullptr) {
+                left->rightLeaf = leaf;
+            }
+            if (right != nullptr) {
+                right->leftLeaf = leaf;
+            }
+
+            // Update start and end of leaf list
+            if (first == nullptr || first->leftLeaf != nullptr) {
+                first = leaf;
+            }
+            if (last == nullptr || last->rightLeaf != nullptr) {
+                last = leaf;
+            }
+            return *leaf;
+        }
 
         size_t add(Key key, Value value, leaf_type*& first, leaf_type*& last)
         {
@@ -95,20 +190,17 @@ namespace osdb
                 if (nodes[i+1].node == nullptr || key < keys[i]) break;
             }
 
-            if (hasLeaves)
+            if (_data.hasLeaves)
             {
-                if (nodes[i].leaf == nullptr)
-                {
-                    nodes[i].leaf = new leaf_type(this, nullptr, nullptr);
-                    first = nodes[i].leaf;
-                    last = nodes[i].leaf;
+                if (nodes[i].leaf == nullptr) {
+                    nodes[i].leaf = &insert_leaf(i, first, last);
                 }
                 return nodes[i].leaf->add(std::move(key), std::move(value));
             }
             else
             {
                 if (nodes[i].node == nullptr) {
-                    nodes[i].node = new bplus_node(this, true, nullptr, nullptr);
+                    nodes[i].node = new bplus_node(this, i, true);
                 }
                 return nodes[i].node->add(std::move(key), std::move(value), first, last);
             }
@@ -122,8 +214,8 @@ namespace osdb
         using iterator_category = std::input_iterator_tag;
         using value_type = typename Leaf::value_type;
         using difference_type = std::ptrdiff_t;
-        using pointer = value_type*;
-        using reference = value_type&;
+        using pointer = const value_type*;
+        using reference = const value_type&;
 
     private:
         Leaf* leaf;
@@ -149,7 +241,7 @@ namespace osdb
 
         leaf_iterator& operator ++() noexcept
         {
-            if (++index == leaf->count)
+            if (++index == leaf->count && leaf->rightLeaf != nullptr)
             {
                 leaf = leaf->rightLeaf;
                 index = 0;
@@ -160,14 +252,28 @@ namespace osdb
         leaf_iterator operator ++(int) noexcept
         {
             leaf_iterator out(leaf, index);
-            if (++index == leaf->count)
+            operator++();
+            return out;
+        }
+
+        leaf_iterator& operator --() noexcept
+        {
+            if (index-- == 0)
             {
-                leaf = leaf->rightLeaf;
-                index = 0;
+                leaf = leaf->leftLeaf;
+                index = leaf->count - 1;
             }
+            return *this;
+        }
+
+        leaf_iterator operator --(int) noexcept
+        {
+            leaf_iterator out(leaf, index);
+            operator--();
             return out;
         }
     };
+
 
     template<typename Leaf>
     class leaf_iterable
@@ -176,6 +282,9 @@ namespace osdb
         size_t start;
         Leaf* last;
         size_t _end;
+
+        template<typename Key, typename Value, size_t Order, size_t LeafSize>
+        friend class bplus_tree;
 
     public:
         leaf_iterable(Leaf* first, size_t start, Leaf* last, size_t end)
@@ -186,6 +295,12 @@ namespace osdb
         }
         leaf_iterator<Leaf> end() noexcept {
             return leaf_iterator<Leaf>(last, _end);
+        }
+        auto rbegin() noexcept {
+            return std::reverse_iterator<leaf_iterator<Leaf>>(end());
+        }
+        auto rend() noexcept {
+            return std::reverse_iterator<leaf_iterator<Leaf>>(begin());
         }
     };
 
@@ -204,15 +319,12 @@ namespace osdb
 
         size_t _height{};
         size_t _size{};
-        node_type root{nullptr, true, nullptr, nullptr};
+        node_type root{nullptr, 0, true};
 
         leaf_type* firstLeaf;
         leaf_type* lastLeaf;
 
     public:
-        bplus_tree() = default;
-
-
         constexpr size_t order() const noexcept {
             return Order;
         }
@@ -229,39 +341,31 @@ namespace osdb
             return _size;
         }
 
-        void add(Key key, Value value)
+        void add(Key key, Value value) &
         {
             _height = root.add(std::move(key), std::move(value),
                 firstLeaf, lastLeaf);
             _size++;
         }
 
-        auto scan_items() const
+        auto search_range(range_start = range_start{}, range_end = range_end{},
+            bool = true, bool = true) const &
         {
-            return leaf_iterable<const leaf_type>(
-                firstLeaf, 0, nullptr, 0);
+            return leaf_iterable<const leaf_type>(firstLeaf, 0,
+                lastLeaf, lastLeaf != nullptr ? lastLeaf->count : 0);
         }
 
-        struct range_start { };
-        struct range_end { };
-
-
-        template<typename T, typename Y>
-        auto search_range(const T& start, const Y& end,
-            bool inclusiveStart = true, bool inclusiveEnd = true) const
+        template<typename T>
+        auto search_range(const T& start, range_end = range_end{},
+            bool inclusiveStart = true, bool = true) const &
         {
             static_assert(!std::is_same<T, range_end>::value,"");
-            static_assert(!std::is_same<Y, range_start>::value,"");
+            auto iter = search_range(range_start{}, range_end{});
 
-            bool fromStart = std::is_same<T, range_start>::value;
-            bool fromEnd = std::is_same<Y, range_end>::value;
-
-            const leaf_type* firstLeaf = fromStart ?
-                this->firstLeaf : find_leaf(start, inclusiveStart);
-            const leaf_type* lastLeaf = fromEnd ?
-                this->lastLeaf : find_leaf(end, inclusiveEnd);
-
+            // Get first leaf and index of first item
             size_t firstIndex = 0;
+            const leaf_type* firstLeaf = find_leaf(start, inclusiveStart);
+
             if (firstLeaf != nullptr)
             {
                 for (; firstIndex < firstLeaf->count; firstIndex++)
@@ -275,8 +379,21 @@ namespace osdb
                     firstIndex = 0;
                 }
             }
-            // Get the index of one past the final item
+            return leaf_iterable<const leaf_type>(
+                firstLeaf, firstIndex, iter.last, iter._end);
+        }
+
+        template<typename T>
+        auto search_range(range_start, const T& end, bool = true,
+            bool inclusiveEnd = true) const &
+        {
+            static_assert(!std::is_same<T, range_start>::value,"");
+            auto iter = search_range(range_start{}, range_end{});
+
+            // Get last leaf and index of one past the final item
             size_t lastIndex = 0;
+            const leaf_type* lastLeaf = find_leaf(end, inclusiveEnd);
+
             if (lastLeaf != nullptr && lastLeaf->count != 0)
             {
                 lastIndex = lastLeaf->count;
@@ -285,14 +402,25 @@ namespace osdb
                     if ((inclusiveEnd && lastLeaf->items[lastIndex-1].first == end) ||
                         lastLeaf->items[lastIndex-1].first < end) break;
                 }
-                if (lastIndex == lastLeaf->count)
+                if (lastIndex == lastLeaf->count && lastLeaf->rightLeaf != nullptr)
                 {
                     lastLeaf = lastLeaf->rightLeaf;
                     lastIndex = 0;
                 }
             }
-            return leaf_iterable<const leaf_type>(firstLeaf,
-                firstIndex, lastLeaf, lastIndex);
+            return leaf_iterable<const leaf_type>(
+                iter.first, iter.start, lastLeaf, lastIndex);
+        }
+
+        template<typename T, typename Y>
+        auto search_range(const T& start, const Y& end,
+            bool inclusiveStart = true, bool inclusiveEnd = true) const &
+        {
+            auto leftIter = search_range(start, range_end{}, inclusiveStart);
+            auto rightIter = search_range(range_start{}, end, true, inclusiveEnd);
+
+            return leaf_iterable<const leaf_type>(
+                leftIter.first, leftIter.start, rightIter.last, rightIter._end);
         }
 
     private:
@@ -308,7 +436,7 @@ namespace osdb
                     else if (!inclusive && (node->keys[i] == value)) continue;
                     else break;
                 }
-                if (node->hasLeaves) return node->nodes[i].leaf;
+                if (node->_data.hasLeaves) return node->nodes[i].leaf;
                 else node = node->nodes[i].node;
             }
             return nullptr;
--- tests/btree-tests.cpp
+++ tests/btree-tests.cpp
@@ -23,7 +23,7 @@ TEST(BtreeSuite, EmptyTest)
     EXPECT_EQ(tree.height(), 0);
     EXPECT_EQ(tree.size(), 0);
 
-    for (auto& pair : tree.scan_items())
+    for (auto& pair : tree.search_range())
     {
         (void)pair;
         ASSERT(false);
@@ -48,14 +48,37 @@ TEST(BtreeSuite, SearchEmpty)
 {
     constexpr const size_t order = 4;
     constexpr const size_t leafSize = 8;
-    constexpr const T1 key{0x5AD};
 
     osdb::bplus_tree<T1, T2, order, leafSize> tree{};
+    using range_start = osdb::range_start;
+    using range_end = osdb::range_end;
 
-    for (auto& pair : tree.search_range(T1{}, T1{}))
-    {
-        (void)pair;
-        ASSERT(false);
+    for (auto& pair : tree.search_range()) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{}, T1{})) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{}, T1{}, false)) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{}, T1{}, true, false)) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{}, T1{}, true, true)) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{})) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(T1{}, range_end{}, false)) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(range_start{}, T1{})) {
+        (void)pair; ASSERT(false);
+    }
+    for (auto& pair : tree.search_range(range_start{}, T1{}, true, false)) {
+        (void)pair; ASSERT(false);
     }
 }
 
@@ -71,6 +94,16 @@ TEST(BtreeSuite, SearchOne)
 
     size_t count = 0;
     for (auto& pair : tree.search_range(key, key))
+    {
+        ASSERT_LT(count, 1);
+        ++count;
+        EXPECT_EQ(pair.first, key);
+        EXPECT_EQ(pair.second, value);
+    }
+    EXPECT_EQ(count, 1);
+
+    count = 0;
+    for (auto& pair : tree.search_range(key))
     {
         ASSERT_LT(count, 1);
         ++count;
@@ -95,6 +128,7 @@ TEST(BtreeSuite, SearchTwo)
     tree.add(key1, value);
     tree.add(key2, value);
 
+    //  == Test for inclusive-bound search ==
     size_t count = 0;
     for (auto& pair : tree.search_range(key1, key2))
     {
@@ -104,6 +138,145 @@ TEST(BtreeSuite, SearchTwo)
         ++count;
     }
     EXPECT_EQ(count, 2);
+
+    count = 0;
+    for (auto& pair : tree.search_range(osdb::range_start{}, key2))
+    {
+        ASSERT_LT(count, 2);
+        EXPECT(pair.first == keys[count]);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 2);
+
+    count = 0;
+    for (auto& pair : tree.search_range(key1))
+    {
+        ASSERT_LT(count, 2);
+        EXPECT(pair.first == keys[count]);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 2);
+
+    //  == Test for exclusive-bound search ==
+    for (auto& pair : tree.search_range(key1, key2, false, false)) {
+        (void)pair; ASSERT(false);
+    }
+
+    count = 0;
+    for (auto& pair : tree.search_range(key1, osdb::range_end{}, false))
+    {
+        ASSERT_LT(count, 1);
+        EXPECT(pair.first == key2);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 1);
+
+    count = 0;
+    for (auto& pair : tree.search_range(osdb::range_start{}, key2, true, false))
+    {
+        ASSERT_LT(count, 1);
+        EXPECT(pair.first == key1);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 1);
+}
+
+#include <tuple>
+template<typename Iterable>
+struct ReversedIterable
+{
+    Iterable& iterable;
+
+    explicit ReversedIterable(Iterable& i)
+        : iterable(i) { }
+
+    auto begin() {
+        return iterable.rbegin();
+    }
+    auto end() {
+        return iterable.rend();
+    }
+};
+
+template<typename Iterable>
+static auto reverse(Iterable&& iterable) {
+    return ReversedIterable<Iterable>(iterable);
+}
+
+TEST(BtreeSuite, SearchTwoReverse)
+{
+    constexpr const size_t order = 4;
+    constexpr const size_t leafSize = 8;
+    constexpr const T1 key1{0x5AD};
+    constexpr const T1 key2{0xC0FFEE};
+    constexpr const T1 value{true};
+
+    std::array<T1, 2> keys{key1, key2};
+    static_assert(key1 < key2,"");
+
+    osdb::bplus_tree<T1, T2, order, leafSize> tree{};
+    tree.add(key1, value);
+    tree.add(key2, value);
+
+    //  == Test for inclusive-bound search ==
+    size_t count = 0;
+    for (auto& pair : reverse(tree.search_range(key1, key2)))
+    {
+        ASSERT_LT(count, 2);
+        EXPECT(pair.first == keys[keys.size() - 1 - count]);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 2);
+
+    count = 0;
+    for (auto& pair : reverse(tree.search_range(osdb::range_start{}, key2)))
+    {
+        ASSERT_LT(count, 2);
+        EXPECT(pair.first == keys[keys.size() - 1 - count]);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 2);
+
+    count = 0;
+    for (auto& pair : reverse(tree.search_range(key1)))
+    {
+        ASSERT_LT(count, 2);
+        EXPECT(pair.first == keys[keys.size() - 1 - count]);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 2);
+
+    //  == Test for exclusive-bound search ==
+    for (auto& pair : reverse(tree.search_range(key1, key2, false, false))) {
+        (void)pair; ASSERT(false);
+    }
+
+    count = 0;
+    for (auto& pair : reverse(tree.search_range(key1, osdb::range_end{}, false)))
+    {
+        ASSERT_LT(count, 1);
+        EXPECT(pair.first == key2);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 1);
+
+    count = 0;
+    for (auto& pair : reverse(tree.search_range(osdb::range_start{}, key2, true, false)))
+    {
+        ASSERT_LT(count, 1);
+        EXPECT(pair.first == key1);
+        EXPECT_EQ(pair.second, value);
+        ++count;
+    }
+    EXPECT_EQ(count, 1);
 }
 
 TEST(BtreeSuite, SearchSame)
@@ -146,7 +319,7 @@ TEST(BtreeSuite, FillLeafSameScan)
     EXPECT_EQ(tree.size(), leafSize);
 
     size_t count = 0;
-    for (auto& pair : tree.scan_items())
+    for (auto& pair : tree.search_range())
     {
         ASSERT_LT(count, leafSize);
         ++count;
